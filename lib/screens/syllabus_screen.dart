e10a729f0ad6a3175842c66b5bb8a5a620bbd249
rizanB <rijann5@gmail.com>
2024-06-01 15:32:01 +0545

    chore: add search in syllabus screen

--- lib/screens/syllabus_screen.dart
+++ lib/screens/syllabus_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:past_qns/widgets/course_heading_section.dart';
 import 'package:past_qns/widgets/syllabus_topic_card.dart';
 
-class SyllabusScreen extends StatelessWidget {
+class SyllabusScreen extends StatefulWidget {
   final List<dynamic> courseSyllabus;
   final String courseName;
   final String courseCode;
@@ -14,10 +14,33 @@ class SyllabusScreen extends StatelessWidget {
       required this.courseSyllabus});
 
   @override
+  State<SyllabusScreen> createState() => _SyllabusScreenState();
+}
+
+class _SyllabusScreenState extends State<SyllabusScreen> {
+  List<dynamic> filteredCourseSyllabus = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredCourseSyllabus = widget.courseSyllabus;
+  }
+
+  void onSearchTextChanged(String searchText) {
+    setState(() {
+      filteredCourseSyllabus = widget.courseSyllabus
+          .where((syllabus) =>
+              syllabus["content"].toLowerCase().contains(searchText.toLowerCase()) ||
+              syllabus["topic"].toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: courseSyllabus.isNotEmpty
+      body: widget.courseSyllabus.isNotEmpty
           ? Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: 8.0,
@@ -26,7 +49,8 @@ class SyllabusScreen extends StatelessWidget {
               child: ListView(
                 children: [
                   CourseHeadingSection(
-                      courseName: courseName, courseCode: courseCode),
+                      courseName: widget.courseName,
+                      courseCode: widget.courseCode),
                   const Padding(
                     padding: EdgeInsets.all(8.0),
                     child: Row(
@@ -44,15 +68,36 @@ class SyllabusScreen extends StatelessWidget {
                       ],
                     ),
                   ),
+                  TextField(
+                    onChanged: onSearchTextChanged,
+                    style: const TextStyle(fontSize: 16),
+                    decoration: InputDecoration(
+                      hintText: "Search in syllabus..",
+                      hintStyle: const TextStyle(color: Colors.grey),
+                      prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                      fillColor: Colors.grey[100],
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              const BorderSide(color: Colors.transparent)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              const BorderSide(color: Colors.transparent)),
+                    ),
+                  ),
                   ListView.builder(
-                    itemCount:
-                        courseSyllabus.length, // Use question list length
+                    itemCount: filteredCourseSyllabus
+                        .length, // Use question list length
                     shrinkWrap: true,
                     physics: const NeverScrollableScrollPhysics(),
                     itemBuilder: (context, index) {
-                      final topic = courseSyllabus[index]["topic"];
-                      final topicContent = courseSyllabus[index]["content"];
-                      int teachingHours = courseSyllabus[index]["hours"];
+                      final topic = filteredCourseSyllabus[index]["topic"];
+                      final topicContent =
+                          filteredCourseSyllabus[index]["content"];
+                      int teachingHours =
+                          filteredCourseSyllabus[index]["hours"];
 
                       return SyllabusTopicCard(
                           topic: topic,
